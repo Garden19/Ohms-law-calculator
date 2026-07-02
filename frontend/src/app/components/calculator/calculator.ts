a68435baba81ffef36daf9e9daceb7a5f14f1d31
EdanR <edanjack1996@hotmail.com>
2026-07-02 20:59:09 +0100

    Added a calculation form with the logic to calculate the users amps and conditionally render css based on the result.

--- frontend/src/app/components/calculator/calculator.ts
+++ frontend/src/app/components/calculator/calculator.ts
@@ -1,11 +1,42 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from "@angular/forms";
+import { NewCalculation } from '../../Models/newCalculation';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './calculator.html',
   styleUrl: './calculator.css',
 })
 export class Calculator {
 
+  isSafe: boolean | null = null;
+  safe = false;
+
+  Calculator = new FormGroup({
+    userVoltage: new FormControl(0, Validators.required),
+    userCoilResistance: new FormControl(0, Validators.required),
+    userBatteryAmpRating: new FormControl(0, Validators.required),
+  })
+
+  onAddCalculation() {
+    const newCalculation: NewCalculation = {
+      userVoltage: this.Calculator.value.userVoltage ?? 0,
+      userCoilResistance: this.Calculator.value.userCoilResistance ?? 0,
+      userBatteryAmpRating: this.Calculator.value.userBatteryAmpRating ?? 0,
+    };
+    
+    let userVoltage = newCalculation.userVoltage;
+    let userCoilResistance = newCalculation.userCoilResistance;
+    let userBatteryAmpRating = newCalculation.userBatteryAmpRating;
+    const userAmps = userVoltage / userCoilResistance;
+    
+    if (userAmps > userBatteryAmpRating) {
+      this.isSafe = false;
+      this.safe = false;
+    } else {
+      this.isSafe = true;
+      this.safe = true;
+    }
+  }
 }
