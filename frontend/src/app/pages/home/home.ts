b90a2bcc2cb00370611d3b38adbf4a43b020b6ad
EdanR <edanjack1996@hotmail.com>
2026-07-02 01:26:53 +0100

    Initial project setup

---
+++ frontend/src/app/pages/home/home.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Calculator } from '../../components/calculator/calculator';
+
+@Component({
+  selector: 'app-home',
+  imports: [Calculator],
+  templateUrl: './home.html',
+  styleUrl: './home.css',
+})
+export class Home {
+
+}
